b02a3021ef1abcfba0efe195f52c92ff31d6b7b9
Mairramer <mairramer.dasilva28@hotmail.com>
2021-06-21 20:29:11 -0300

    text styles adicionados

---
+++ lib/src/shared/consts/app_text_styles_const.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:nlw_payflow/src/shared/consts/app_colors_const.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  static final titleHome = GoogleFonts.lexendDeca(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+    color: AppColorsConst.heading,
+  );
+  static final titleRegular = GoogleFonts.lexendDeca(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    color: AppColorsConst.heading,
+  );
+
+  static final titleBoldHeading = GoogleFonts.lexendDeca(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AppColorsConst.heading,
+  );
+
+  static final titleBoldBackground = GoogleFonts.lexendDeca(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AppColorsConst.background,
+  );
+
+  static final titleListTile = GoogleFonts.lexendDeca(
+    fontSize: 17,
+    fontWeight: FontWeight.w600,
+    color: AppColorsConst.heading,
+  );
+
+  static final trailingRegular = GoogleFonts.lexendDeca(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColorsConst.heading,
+  );
+
+  static final trailingBold = GoogleFonts.lexendDeca(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: AppColorsConst.heading,
+  );
+
+  static final buttonPrimary = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColorsConst.primary,
+  );
+  static final buttonHeading = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColorsConst.heading,
+  );
+  static final buttonGray = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColorsConst.gray,
+  );
+  static final buttonBackground = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColorsConst.background,
+  );
+
+  static final buttonBoldPrimary = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColorsConst.primary,
+  );
+  static final buttonBoldHeading = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColorsConst.heading,
+  );
+  static final buttonBoldGray = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColorsConst.gray,
+  );
+  static final buttonBoldBackground = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColorsConst.background,
+  );
+
+  static final captionBackground = GoogleFonts.lexendDeca(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    color: AppColorsConst.background,
+  );
+
+  static final captionShape = GoogleFonts.lexendDeca(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    color: AppColorsConst.shape,
+  );
+
+  static final captionBody = GoogleFonts.lexendDeca(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    color: AppColorsConst.body,
+  );
+
+  static final captionBoldBackground = GoogleFonts.lexendDeca(
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+    color: AppColorsConst.background,
+  );
+
+  static final captionBoldShape = GoogleFonts.lexendDeca(
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+    color: AppColorsConst.shape,
+  );
+
+  static final captionBoldBody = GoogleFonts.lexendDeca(
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+    color: AppColorsConst.body,
+  );
+}
