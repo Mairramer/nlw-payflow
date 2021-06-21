4cd4f1b22d8ccdc2d1f44e9f524856438b3be1cc
Mairramer <mairramer.dasilva28@hotmail.com>
2021-06-21 20:28:43 -0300

    imagens adicionadas

---
+++ lib/src/shared/consts/app_images_const.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppImages {
+  static const String images = 'assets/images/';
+  static const logoFull = "'${images}logofull.png";
+  static const logomini = "'${images}logomini.png";
+  static const union = "'${images}union.png";
+  static const person = "'${images}person.png";
+  static const google = "'${images}google.png";
+}
