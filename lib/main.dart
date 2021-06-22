bfa03caa9d2b0279050514a0d134e1d860321999
Mairramer <mairramer.dasilva28@hotmail.com>
2021-06-21 21:36:21 -0300

    MyApp alterado para AppWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:nlw_payflow/src/modules/splash/splash_page.dart';
+import 'package:nlw_payflow/src/shared/consts/app_colors_const.dart';
+
+import 'src/modules/home/home_page.dart';
+import 'src/modules/login/login_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(AppWidget());
 }
 
-class MyApp extends StatelessWidget {
+class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pay Flow',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: AppColorsConst.primary,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      home: HomePage(),
     );
   }
 }
